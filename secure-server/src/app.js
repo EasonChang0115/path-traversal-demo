94c84e52ee1a32149abd54bb51ecdfe2947d1b3d
jayden <jayden@verizonmedia.com>
2021-09-05 15:04:00 +0800

    use json server

--- secure-server/src/app.js
+++ secure-server/src/app.js
@@ -1,22 +1,11 @@
-import express from 'express';
-// 路由
-import systeminfo from './route/systeminfo';
-import posts from './route/posts';
-const app = express();
-// res body 的解析
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('./secure-server/src/db.json')
+const middlewares = jsonServer.defaults()
 
-// 載入路由
-app.use('/systeminfo', systeminfo);
-app.use('/posts', posts);
-app.use((req, res, next) => {
-  console.log(req.originalUrl);
-  res.status(404).send('<h1>Page not found on the server</h1>');
-});
-
-// 開啟專案
+server.use(middlewares)
+server.use(router)
 const port = 3001;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('http://127.0.0.1:3001/posts');
-});
+})
